baef9f5f1f14e0927cacd38c0d78062aabdab08a
Карина Гордеева <kagordeeva_1@miem.hse.ru>
2021-07-27 22:46:05 +0500

    feat: added extra info about user, permissions table

--- database.py
+++ database.py
@@ -5,6 +5,8 @@
 from pony.orm import *
 from datetime import datetime
 import config
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
 
 date_format = '%d.%m.%y %H:%M:%S'
 
@@ -12,15 +14,24 @@ db = Database()
 
 
 class User(db.Entity):
+    first_name = Required(str)
+    last_name = Required(str)
     chat_id = Required(int)
     permission = Required(int)
     KMLink = Required(str)
     date = Required(str)
 
+
 class Text(db.Entity):
     permission = Required(int)
     key = Required(str)
     message = Required(str)
+    date = Required(str)
+
+
+class Permission(db.Entity):
+    key = Required(int)
+    name = Required(str)
     date = Required(str)
 
 
@@ -30,20 +41,29 @@ db.generate_mapping(create_tables=True)
 
 
 @db_session
-def add_users(users: set) -> None:  # {chat_id}
+def add_users(users: set) -> None:
+    vk_session = vk_api.VkApi(token=config.TOKEN)
+    longpoll = VkLongPoll(vk_session)
+    vk = vk_session.get_api()
+
     old_users = set(select(user.chat_id for user in User))
     old_admins = set(select(user.chat_id for user in User if user.permission == 0))
 
     for user in users | config.admins:
         if user not in old_users:
             admin = int(not (user in config.admins | old_admins))
+            user_info = vk.users.get(user_id=user)
+            user_info = user_info[0]
             User(
+                first_name=user_info['first_name'],
+                last_name=user_info['last_name'],
                 chat_id=user,
                 permission=admin,
                 KMLink='vk.com/' + config.headLogin,
                 date=datetime.now().strftime(date_format)
             )
             old_users.add(user)
+
 
 @db_session
 def add_texts(texts) -> None:
@@ -60,7 +80,22 @@ def add_texts(texts) -> None:
             old_keys.add(key)
 
 
+@db_session
+def add_permissions(permissions: dict) -> None:
+    old_names = set(select(permission.name for permission in Permission))
+
+    for key in permissions.keys():
+        name = permissions[key]
+        if name not in old_names:
+            Permission(
+                key=key,
+                name=name,
+                date=datetime.now().strftime(date_format)
+            )
+            old_names.add(name)
+
+
 add_users(config.admins)
 add_texts(config.texts)
-
+add_permissions(config.permissions)
 commit()
