f3bb04daa1e54e128ed478bacfb09382796f55a4
Карина Гордеева <kagordeeva_1@edu.hse.ru>
2021-10-26 14:38:17 +0300

    feat: update new_title, send_message command

--- commandHandler.py
+++ commandHandler.py
@@ -1,3 +1,5 @@
+import json
+
 from vk_api.longpoll import Event
 from vk_api.longpoll import VkLongPoll, VkEventType
 import vk_api
@@ -14,6 +16,7 @@ vk_session = vk_api.VkApi(token=TOKEN)
 longpoll = VkLongPoll(vk_session)
 vk = vk_session.get_api()
 
+
 # args = [text.title]
 def new_title(event: Event, args: List[str]) -> int:
     if not args or not args[0]:
@@ -21,6 +24,15 @@ def new_title(event: Event, args: List[str]) -> int:
 
     # Подключение к БД
     session = get_session(engine)
+
+    title = args[0]
+
+    titles = {text.title for text in session.query(Text)}
+    if title in titles:
+        # Завершение работы в БД
+        session.close()
+
+        return 3
 
     session.add(Text(title=args[0], date=datetime.now()))
 
@@ -41,11 +53,7 @@ def send_message(event: Event, args: List[str]) -> int:
 
     params = {'title': args[0]}
     if len(args) > 1 and args[1].isdigit(): params['step'] = int(args[1])
-    elif args[1]: params['step'] = session.query(Step.number).filter_by(name=args[1]).first().number
-    print(params)
-
-    if not params['step'] and params['step'] != 0:
-        return 2
+    elif len(args) > 1 and args[1]: params['step'] = session.query(Step.number).filter_by(name=args[1]).first().number
 
     text = session.query(Text).filter_by(**params).first()
 
@@ -57,6 +65,16 @@ def send_message(event: Event, args: List[str]) -> int:
                 message=text.text,
                 attachment=text.attachment
             )
+
+    else:
+        # Завершение работы в БД
+        session.close()
+
+        return 2
+
+    # Завершение работы в БД
+    session.commit()
+    session.close()
 
     return 0
 
