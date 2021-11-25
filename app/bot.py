60310e7392182fff5c073c01315590e7c9a634da
Egor Danilov <danilov6083@outlook.com>
2021-11-25 17:47:24 +0300

    feat: added full realization of Info and Red buttons

--- app/bot.py
+++ app/bot.py
@@ -12,7 +12,7 @@ from config import settings
 from create_tables import engine, get_session, User, Text, Command
 import sending
 import commandHandler
-
+import vkKeyboard
 
 # Подключение к сообществу
 vk_session = vk_api.VkApi(token=settings.VK_BOT_TOKEN)
@@ -74,6 +74,9 @@ def start():
                     text=message
                 )
             session.close()
+
+    #булевая переменная для отслеживания того, что была нажата "красная кнопка"
+    pushed_problem_button = False
 
     # Зацикливание запуска прослушки после возможных исключений
     while True:
@@ -148,152 +151,198 @@ def start():
 
                         continue
 
-                    # Если ввели текстовое сообщение, то игнорировать:
-                    if event.message['text'][0] not in {'/', '!', '.'}:
+                    # отправка сообщения о проблеме от юзера главному админу
+                    if pushed_problem_button and event.message['text']:
 
-                        # Завершение работы с БД
-                        session.commit()
-                        session.close()
+                        domain = session.query(User.domain).filter_by(chat_id=event.message['from_id']).first()
 
-                        # Задержка от спама
-                        time.sleep(settings.DELAY)
+                        sending.message(vk=vk,
+                                        text=f'Получено сообщение об ошибке от пользователя vk.com/{domain[0]} ' \
+                                        f'({event.message["from_id"]})' \
+                                        f'\nОписание проблемы: ' + event.message['text'],
+                                        chat_id=settings.MY_VK_ID)
 
-                        continue
+                        sending.message(vk=vk,
+                                        text='Уведомление об ошибке отправлено' \
+                                             '\nС вами обязательно свяжутся',
+                                        chat_id=event.message['from_id'])
 
-                    # Разделение текста сообщения на команду
-                    words = list(map(str, event.message['text'].split()))
-                    command = words[0][1:].lower()
-                    logger.info(f'user_id="{event.message["from_id"]}" вызвал команду "{command}"')
+                        pushed_problem_button = False
 
-                    # Если ввели несуществующую команду:
-                    if not session.query(Command).filter_by(name=command).first():
-                        sending.message(
-                            vk=vk,
-                            chat_id=event.message['from_id'],
-                            text='Такой команды не существует.'
-                        )
+                    elif event.message['text'] == 'Начать':
 
-                        continue
+                        admin = session.query(User.admin).filter_by(chat_id=event.message['from_id']).first()
 
-                    # Если есть необходимые права:
-                    if (session.query(User).filter_by(chat_id=event.message['from_id']).first().admin or
-                            not session.query(Command).filter_by(name=command).first().admin):
+                        sending.message(vk=vk,
+                                        text='Просто нажми на нужную кнопку :)',
+                                        chat_id=event.message['from_id'],
+                                        keyboard=vkKeyboard.get_admin_keyboard() if admin \
+                                        else vkKeyboard.get_user_keyboard())
 
-                        # Отправить подтверждение начала выполнения команды
-                        sending.message(
-                            vk=vk,
-                            chat_id=event.message['from_id'],
-                            text=f'Вызвана команда:\n{command}. Начинаю выполнять...'
-                        )
+                    else:
 
-                        # Разделение текста сообщения на аргументы команды, если они введены
-                        args = re.findall('<(.*?)>', event.message['text'], re.DOTALL)
+                        # Если ввели текстовое сообщение, то игнорировать:
+                        if event.message['text'][0] not in {'/', '!', '.'}:
 
-                        # Обработчик команд:
-                        response = -1
-                        handler = commandHandler.Handler()
+                            # Завершение работы с БД
+                            session.commit()
+                            session.close()
 
-                        # new_title "{text.title}"
-                        if command == 'new_title':
-                            response = handler.new_title(event, args)
-                            # response = vkCommandHandler.new_next(event, args)
+                            # Задержка от спама
+                            time.sleep(settings.DELAY)
 
-                        # load
-                        elif command == 'load':
-                            response = handler.load(event, args)
+                            continue
 
-                        # send_message "{text.title}" "OPTIONALLY: {step.number} | {step.name}"
-                        elif command == 'send_message':
-                            response = handler.send_message(event, args)
+                        # Разделение текста сообщения на команду
+                        words = list(map(str, event.message['text'].split()))
+                        command = words[0][1:].lower()
+                        logger.info(f'user_id="{event.message["from_id"]}" вызвал команду "{command}"')
 
-                        # update_text "{text.title}" "text.text"
-                        elif command == 'update_text':
-                            response = handler.update_text(event, args)
-
-                        # update_attachment "{text.title}" "{text.attachments}"
-                        elif command == 'update_attachment':
-                            response = handler.update_attachment(event, args)
-
-                        # add_text_attachments "{text.title}" "{text.attachments}"
-                        elif command == 'add_text_attachments':
-                            response = handler.add_text_attachments(event, args)
-
-                        # delete_text_attachments "{text.title}" "{text.attachments}"
-                        elif command == 'delete_text_attachments':
-                            response = handler.delete_text_attachments(event, args)
-
-                        # delete_attachments "{text.title}"
-                        elif command == 'clear_text_attachments':
-                            response = handler.clear_text_attachments(event, args)
-
-                        # update_text_step "{text.title}" "{step.number} | {step.name}"
-                        elif command == 'update_text_step':
-                            response = handler.update_text_step(event, args)
-
-                        # update_user_step "{user.chat_id} | {user.domain}" "{step.number} | {step.name}"
-                        elif command == 'update_user_step':
-                            response = handler.update_user_step(event, args)
-
-                        # get_commands
-                        elif command == 'get_commands':
-                            response = handler.get_commands(event, args)
-
-                        # update_user_admin "{user.domain}" "{user.admin}"
-                        elif command == 'update_user_admin':
-                            response = handler.update_user_admin(event, args)
-
-                        # check
-                        elif command == 'check':
-                            response = handler.check(event, args)
-
-                        # get_texts
-                        elif command == 'get_texts':
-                            response = handler.get_texts(event, args)
-
-                        # get_steps
-                        elif command == 'get_steps':
-                            response = handler.get_steps(event, args)
-
-                        # get_users
-                        elif command == 'get_users':
-                            response = handler.get_users(event, args)
-
-                        # copy_texts
-                        elif command == 'copy_text':
-                            response = handler.copy_text(event, args)
-
-                        # copy_users
-                        elif command == 'copy_user':
-                            response = handler.copy_user(event, args)
-
-                        # copy_step
-                        elif command == 'copy_step':
-                            response = handler.copy_step(event, args)
-
-                        # copy_attachment
-                        elif command == 'copy_attachment':
-                            response = handler.copy_attachment(event, args)
-
-                        # copy_command
-                        elif command == 'copy_command':
-                            response = handler.copy_command(event, args)
-
-                        # copy
-                        elif command == 'copy':
-                            response = handler.copy(event, args)
-
-                        handler.session.close()
-                        del handler
-
-                        if response:
-
-                            # Отправить уведомление о некорректном завершении работы команды
+                        # Если ввели несуществующую команду:
+                        if not session.query(Command).filter_by(name=command).first():
                             sending.message(
                                 vk=vk,
                                 chat_id=event.message['from_id'],
-                                text=f'=== Команда "{command}" не выполнена. ===\n'
-                                     f'{errors[response]}\n'
-                                     f'========================================='
+                                text='Такой команды не существует.'
+                            )
+
+                            continue
+
+                        # Если есть необходимые права:
+                        if (session.query(User).filter_by(chat_id=event.message['from_id']).first().admin or
+                                not session.query(Command).filter_by(name=command).first().admin):
+
+                            # Отправить подтверждение начала выполнения команды
+                            sending.message(
+                                vk=vk,
+                                chat_id=event.message['from_id'],
+                                text=f'Вызвана команда:\n{command}. Начинаю выполнять...'
+                            )
+
+                            # Разделение текста сообщения на аргументы команды, если они введены
+                            args = re.findall('<(.*?)>', event.message['text'], re.DOTALL)
+
+                            # Обработчик команд:
+                            response = -1
+                            handler = commandHandler.Handler()
+
+                            # new_title "{text.title}"
+                            if command == 'new_title':
+                                response = handler.new_title(event, args)
+                                # response = vkCommandHandler.new_next(event, args)
+
+                            # load
+                            elif command == 'load':
+                                response = handler.load(event, args)
+
+                            # send_message "{text.title}" "OPTIONALLY: {step.number} | {step.name}"
+                            elif command == 'send_message':
+                                response = handler.send_message(event, args)
+
+                            # update_text "{text.title}" "text.text"
+                            elif command == 'update_text':
+                                response = handler.update_text(event, args)
+
+                            # update_attachment "{text.title}" "{text.attachments}"
+                            elif command == 'update_attachment':
+                                response = handler.update_attachment(event, args)
+
+                            # add_text_attachments "{text.title}" "{text.attachments}"
+                            elif command == 'add_text_attachments':
+                                response = handler.add_text_attachments(event, args)
+
+                            # delete_text_attachments "{text.title}" "{text.attachments}"
+                            elif command == 'delete_text_attachments':
+                                response = handler.delete_text_attachments(event, args)
+
+                            # delete_attachments "{text.title}"
+                            elif command == 'clear_text_attachments':
+                                response = handler.clear_text_attachments(event, args)
+
+                            # update_text_step "{text.title}" "{step.number} | {step.name}"
+                            elif command == 'update_text_step':
+                                response = handler.update_text_step(event, args)
+
+                            # update_user_step "{user.chat_id} | {user.domain}" "{step.number} | {step.name}"
+                            elif command == 'update_user_step':
+                                response = handler.update_user_step(event, args)
+
+                            # get_commands
+                            elif command == 'get_commands':
+                                response = handler.get_commands(event, args)
+
+                            # update_user_admin "{user.domain}" "{user.admin}"
+                            elif command == 'update_user_admin':
+                                response = handler.update_user_admin(event, args)
+
+                            # check
+                            elif command == 'check':
+                                response = handler.check(event, args)
+
+                            # get_texts
+                            elif command == 'get_texts':
+                                response = handler.get_texts(event, args)
+
+                            # get_steps
+                            elif command == 'get_steps':
+                                response = handler.get_steps(event, args)
+
+                            # get_users
+                            elif command == 'get_users':
+                                response = handler.get_users(event, args)
+
+                            # copy_texts
+                            elif command == 'copy_text':
+                                response = handler.copy_text(event, args)
+
+                            # copy_users
+                            elif command == 'copy_user':
+                                response = handler.copy_user(event, args)
+
+                            # copy_step
+                            elif command == 'copy_step':
+                                response = handler.copy_step(event, args)
+
+                            # copy_attachment
+                            elif command == 'copy_attachment':
+                                response = handler.copy_attachment(event, args)
+
+                            # copy_command
+                            elif command == 'copy_command':
+                                response = handler.copy_command(event, args)
+
+                            # copy
+                            elif command == 'copy':
+                                response = handler.copy(event, args)
+
+                            handler.session.close()
+                            del handler
+
+                            if response:
+
+                                # Отправить уведомление о некорректном завершении работы команды
+                                sending.message(
+                                    vk=vk,
+                                    chat_id=event.message['from_id'],
+                                    text=f'=== Команда "{command}" не выполнена. ===\n'
+                                         f'{errors[response]}\n'
+                                         f'========================================='
+                                )
+
+                                # Завершение работы в БД
+                                session.commit()
+                                session.close()
+
+                                # Задержка от спама
+                                time.sleep(settings.DELAY)
+
+                                continue
+
+                            # Отправить уведомление об успешном завершении работы команды
+                            sending.message(
+                                vk=vk,
+                                chat_id=event.message['from_id'],
+                                text='Команда успешно выполнена.'
                             )
 
                             # Завершение работы в БД
@@ -303,29 +352,27 @@ def start():
                             # Задержка от спама
                             time.sleep(settings.DELAY)
 
-                            continue
+                        # Сообщение о вызове недоступной команды
+                        else:
+                            sending.message(
+                                vk=vk,
+                                chat_id=event.message['from_id'],
+                                text=f'Недостаточно прав.'
+                            )
 
-                        # Отправить уведомление об успешном завершении работы команды
-                        sending.message(
-                            vk=vk,
-                            chat_id=event.message['from_id'],
-                            text='Команда успешно выполнена.'
-                        )
+                elif event.type == VkBotEventType.MESSAGE_EVENT:
 
-                        # Завершение работы в БД
-                        session.commit()
-                        session.close()
+                    if event.object['payload'][0] == 'Info':
+                        sending.message(vk=vk,
+                                        chat_id=event.object['user_id'],
+                                        text='Антипосвят')
 
-                        # Задержка от спама
-                        time.sleep(settings.DELAY)
+                    elif event.object['payload'][0] == 'Problem':
 
-                    # Сообщение о вызове недоступной команды
-                    else:
-                        sending.message(
-                            vk=vk,
-                            chat_id=event.message['from_id'],
-                            text=f'Недостаточно прав.'
-                        )
+                        pushed_problem_button = True
+                        sending.message(vk=vk,
+                                        chat_id=event.object['user_id'],
+                                        text='Максимально лаконично опишите свою проблему')
 
                 # Задержка от спама
                 time.sleep(settings.DELAY)
--- app/vkKeyboard.py
+++ app/vkKeyboard.py
@@ -1,10 +1,39 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from vk_api.longpoll import Event
-from vk_api.keyboard import VkKeyboard, VkKeyboardColor, VkKeyboardButton
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 import vk_api
 
-keyboard = vk_api.keyboard.VkKeyboard(inline=True)
-keyboard.add_callback_button(label='FAQ',color=VkKeyboardColor.PRIMARY)
+def get_user_keyboard() -> vk_api.keyboard.VkKeyboard:
 
+    keyboard = vk_api.keyboard.VkKeyboard(inline=True)
+    keyboard.add_callback_button(label='Информация',
+                                 color=VkKeyboardColor.PRIMARY,
+                                 payload=['Info'])
+    keyboard.add_line()
+    keyboard.add_callback_button(label='Сообщить о проблеме',
+                                 color=VkKeyboardColor.PRIMARY,
+                                 payload=['Problem'])
+
+    return keyboard
+
+def get_admin_keyboard() -> vk_api.keyboard.VkKeyboard:
+
+    keyboard = vk_api.keyboard.VkKeyboard(inline=True)
+    keyboard.add_callback_button(label='Вызов команд',
+                                 color=VkKeyboardColor.POSITIVE,
+                                 payload=['Commands'])
+    keyboard.add_line()
+    keyboard.add_callback_button(label='Информация',
+                                 color=VkKeyboardColor.PRIMARY,
+                                 payload=['Info'])
+    keyboard.add_line()
+    keyboard.add_callback_button(label='Сообщить о проблеме',
+                                 color=VkKeyboardColor.PRIMARY,
+                                 payload=['Problem'])
+    keyboard.add_line()
+    keyboard.add_callback_button(label='Выключить бота',
+                                 color=VkKeyboardColor.NEGATIVE,
+                                 payload=['Turn off'])
+
+    return keyboard
